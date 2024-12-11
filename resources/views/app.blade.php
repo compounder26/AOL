dbcdbe21c7b2e045e8bf97b3e134e7a7b20ccc13
Tertius <94773757+compounder26@users.noreply.github.com>
2024-12-11 20:59:59 +0700

    newBrand5

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="build/assets/app.css">
-    <link rel="stylesheet" href="build/assets/style_thad.css">
+    <link rel="stylesheet" href="/build/assets/app.css">
+    <link rel="stylesheet" href="/build/assets/style_thad.css">
 </head>
 <body>
     <div class="min-vh-120 d-flex flex-column" style="overflow-x: hidden; margin: 0; padding: 0; width: 100%; max-width: 2000px;">
@@ -17,6 +17,6 @@
             @yield('footer')
         </div>
     </div>
-    <script src="build/assets/app.js"></script>
+    <script src="/build/assets/app.js"></script>
 </body>
 </html>
